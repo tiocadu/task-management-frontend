233834dc54cea5c778c0356ad503df04c19bf7c9
tiocadu <tiocadu@users.noreply.github.com>
2021-08-29 16:05:03 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.a2d809cdfb314df135aad56b0c52f5d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aceb5564da1019fe6f572fa85a1fc49d",
+    "url": "https://tiocadu.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "604db11d2c444455363e",
+    "url": "https://tiocadu.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "eae4d45705ea75f0e774",
+    "url": "https://tiocadu.github.io/task-management-frontend/static/js/2.3ded181f.chunk.js"
+  },
+  {
+    "revision": "604db11d2c444455363e",
+    "url": "https://tiocadu.github.io/task-management-frontend/static/js/main.53ecec26.chunk.js"
+  },
+  {
+    "revision": "362e6618bcfcd848471c",
+    "url": "https://tiocadu.github.io/task-management-frontend/static/js/runtime~main.b7c67667.js"
+  }
+]);
